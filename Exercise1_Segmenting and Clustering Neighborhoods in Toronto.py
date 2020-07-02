5d18c6e3bfbf4ed7d620a2e980dc6bc303c301d8
marinagbastos <67479287+marinagbastos@users.noreply.github.com>
2020-07-01 21:33:44 -0300

    Update Exercise1_Segmenting and Clustering Neighborhoods in Toronto.py

--- Exercise1_Segmenting and Clustering Neighborhoods in Toronto.py
+++ Exercise1_Segmenting and Clustering Neighborhoods in Toronto.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun 30 22:07:51 2020
-
-@author: marin
-"""
 #Importing table from HTML 
 
 import requests
@@ -22,7 +16,13 @@ df_canada = df_canada.drop(df_canada[df_canada.Borough=="Not assigned"].index)
 
 df_canada=df_canada.groupby("Postal Code").agg(lambda x:','.join(set(x)))
 
-
 #If a cell has a borough but a Not assigned neighborhood, then the neighborhood will be the same as the borough
+
 df_canada.loc[df_canada.Neighborhood == "Not assigned", "Neighborhood"] = df_canada.Borough
+
+# The dataframe will consist of three columns: PostalCode, Borough, and Neighborhood
+
+df_canada.reset_index(level=0, inplace=True)
+
+df_canada.columns=['Postcode','Borough','Neighbourhood']
 df_canada.shape
